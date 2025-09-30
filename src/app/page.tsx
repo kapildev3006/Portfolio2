b0ecb36c432c9e2c06c60a9218ab3ae6a2c66634
Kapil Dev <kdev7830@gmail.com>
2025-09-30 13:56:59 +0000

    when we at the home if scroll then all pages present in the scroll but i

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,8 @@
 import Header from '@/components/header';
 import Hero from '@/components/sections/hero';
+import About from '@/components/sections/about';
+import Projects from '@/components/sections/projects';
+import Contact from '@/components/sections/contact';
 import Footer from '@/components/footer';
 
 export default function Home() {
@@ -8,6 +11,9 @@ export default function Home() {
       <Header />
       <main className="flex-1">
         <Hero />
+        <About />
+        <Projects />
+        <Contact />
       </main>
       <Footer />
     </div>
