0929ba9646d2c245361e7f17044f1095ecd7143e
Kapil Dev <kdev7830@gmail.com>
2025-10-01 08:35:16 +0000

    Try fixing this error: `Runtime Error: Error: useSidebar must be used wi

--- src/app/admin/(dashboard)/layout.tsx
+++ src/app/admin/(dashboard)/layout.tsx
@@ -1,4 +1,3 @@
-
 'use client';
 
 import AdminSidebar from '@/components/admin/sidebar';
@@ -8,6 +7,7 @@ import { useEffect } from 'react';
 import { Loader2 } from 'lucide-react';
 import AdminHeader from '@/components/admin/header';
 import { PortfolioDataProvider } from '@/context/PortfolioDataProvider';
+import { SidebarProvider } from '@/components/ui/sidebar';
 
 export default function DashboardLayout({
   children,
@@ -32,12 +32,14 @@ export default function DashboardLayout({
   }
 
   return (
-    <PortfolioDataProvider>
-        <AdminSidebar />
-        <main className="flex-1 md:ml-[var(--sidebar-width)] group-data-[collapsible=icon]/sidebar-wrapper:md:ml-[var(--sidebar-width-icon)] transition-all duration-200 ease-linear">
-          <AdminHeader />
-          {children}
-        </main>
-    </PortfolioDataProvider>
+    <SidebarProvider>
+        <PortfolioDataProvider>
+            <AdminSidebar />
+            <main className="flex-1 md:ml-[var(--sidebar-width)] group-data-[collapsible=icon]/sidebar-wrapper:md:ml-[var(--sidebar-width-icon)] transition-all duration-200 ease-linear">
+              <AdminHeader />
+              {children}
+            </main>
+        </PortfolioDataProvider>
+    </SidebarProvider>
   );
 }
